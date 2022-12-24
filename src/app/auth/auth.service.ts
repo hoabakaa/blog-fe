67f265eb0d45b015eac6cdae7fb679090cbd355d
harusora <longcnhe141293@gmail.com>
2022-12-24 09:57:41 +0700

    add login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { RegisterPayload } from './register-payload';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { LoginPayload } from './login-payload';
+import { JwtAuthResponse } from './jwt-auth-response';
+import {LocalStorageService} from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,17 @@ import { Observable } from 'rxjs';
 export class AuthService {
   private url = 'http://localhost:8080/api/auth/'
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private localStorageService: LocalStorageService) { }
 
   register(registerPayload: RegisterPayload): Observable<any>{
-    return this.httpClient.post(this.url + "signup", registerPayload);
+    return this.httpClient.post(this.url + 'signup', registerPayload);
+  }
+
+  login(loginPayload: LoginPayload): Observable<any>{
+    return this.httpClient.post<JwtAuthResponse>(this.url + 'login', loginPayload).pipe(map(data => {
+      this.localStorageService.store('token', data.token);
+      this.localStorageService.store('username', data.username);
+      return true;
+    }));
   }
 }
---
+++ src/app/auth/jwt-auth-response.ts
@@ -0,0 +1,4 @@
+export class JwtAuthResponse {
+  token!: string;
+  username!: string;
+}
