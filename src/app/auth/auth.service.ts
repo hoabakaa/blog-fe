5a0ec0aa0f29e48d4fa37a7f8e1c8bb877c7bce5
Harusora <longcnhe141293@gmail.com>
2023-01-08 09:59:55 +0700

    logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,32 +1,44 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { RegisterPayload } from './register-payload';
 import { map, Observable } from 'rxjs';
 import { LoginPayload } from './login-payload';
 import { JwtAuthResponse } from './jwt-auth-response';
-import {LocalStorageService} from 'ngx-webstorage';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private url = 'http://localhost:8080/api/auth/'
+  private url = 'http://localhost:8080/api/auth/';
 
-  constructor(private httpClient: HttpClient, private localStorageService: LocalStorageService) { }
+  constructor(
+    private httpClient: HttpClient,
+    private localStorageService: LocalStorageService
+  ) {}
 
-  register(registerPayload: RegisterPayload): Observable<any>{
+  register(registerPayload: RegisterPayload): Observable<any> {
     return this.httpClient.post(this.url + 'signup', registerPayload);
   }
 
-  login(loginPayload: LoginPayload): Observable<any>{
-    return this.httpClient.post<JwtAuthResponse>(this.url + 'login', loginPayload).pipe(map(data => {
-      this.localStorageService.store('token', data.token);
-      this.localStorageService.store('username', data.username);
-      return true;
-    }));
+  login(loginPayload: LoginPayload): Observable<any> {
+    return this.httpClient
+      .post<JwtAuthResponse>(this.url + 'login', loginPayload)
+      .pipe(
+        map((data) => {
+          this.localStorageService.store('token', data.token);
+          this.localStorageService.store('username', data.username);
+          return true;
+        })
+      );
   }
 
   isAuthenticated(): Boolean {
     return this.localStorageService.retrieve('username') != null;
+  }
+
+  logout() {
+    this.localStorageService.clear('token');
+    this.localStorageService.clear('username');
   }
 }
